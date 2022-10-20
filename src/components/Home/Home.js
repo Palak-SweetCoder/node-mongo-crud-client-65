151638d06b2c72d221a8bf1ea0c468e40bfa4e8e
Palak-SweetCoder <pspalak6@gmail.com>
2022-10-20 14:34:21 +0600

    user delete crud operation done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-    return(
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, []);
+
+    const handleUserDelete = id => {
+        const proced = window.confirm('Are you sure you want to delete?');
+        if (proced) {
+            console.log('Deleting user with id:', id);
+            const url = `http://localhost:5000/user/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted');
+                        const remaining = users.filter(u => u._id !== id);
+                        setUsers(remaining);
+                    }
+                })
+        }
+    }
+
+    return (
         <div>
             <h1>This is our Home Page</h1>
+            <p>Total users: {users.length}</p>
+            <ul>
+                {
+                    users.map(u => <li key={u._id}>Name: {u.name}
+                        <button onClick={() => handleUserDelete(u._id)}>x</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
